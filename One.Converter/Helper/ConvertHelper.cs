b9e3227979e53b91fe8fd3638b478089b7d484dc
beytullah.buyuktolu@teknosol.com.tr <beytullah.buyuktolu@teknosol.com.tr>
2024-08-22 14:51:51 +0300

    first commit

---
+++ One.Converter/Helper/ConvertHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace One.Converter.Helper;
+
+public class ConvertHelper
+{
+
+    public static string ExportToPem(X509Certificate2 cert)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.AppendLine("-----BEGIN CERTIFICATE-----");
+        builder.AppendLine(Convert.ToBase64String(cert.RawData, Base64FormattingOptions.InsertLineBreaks));
+        builder.AppendLine("-----END CERTIFICATE-----");
+        return builder.ToString();
+    }
+
+    //Extract Private Key in PEM format 
+    public static string ExportPrivateKey(X509Certificate2 cert)
+    {
+        var rsa = cert.GetRSAPrivateKey();
+        if (rsa == null) return null;
+        var builder = new StringBuilder();
+        builder.AppendLine("-----BEGIN PRIVATE KEY-----");
+        builder.AppendLine(Convert.ToBase64String(rsa.ExportPkcs8PrivateKey(), Base64FormattingOptions.InsertLineBreaks));
+        builder.AppendLine("-----END PRIVATE KEY-----");
+        return builder.ToString();
+    }
+}
---
+++ One.Converter/Program.cs
@@ -0,0 +1,31 @@
+﻿using One.Converter.Helper;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Specify the path and password of the PFX file
+        string pfxFilePath = @"C:\ssl\SSLPfxFile.pfx";
+        string pfxConvertPath = @"C:\ssl\";
+        string pfxPassword = "SSLPfxFilePassword";
+
+        // Load PFX file
+        var cert = new X509Certificate2(pfxFilePath, pfxPassword, X509KeyStorageFlags.Exportable);
+
+        // Extract and save the CRT file
+        var crt = ConvertHelper.ExportToPem(cert);
+        File.WriteAllText(pfxConvertPath + "certificate.crt", crt);
+
+
+        // Extract and save the private.key file
+        var privateKey = ConvertHelper.ExportPrivateKey(cert);
+        if (privateKey != null)
+        {
+            File.WriteAllText(pfxConvertPath + "private.key", privateKey);
+            Console.WriteLine("CRT and KEY files created successfully.");
+        }
+        else
+            Console.WriteLine("The private key was not found or is inaccessible.");
+    }
+}
